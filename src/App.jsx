c3b3fc1f4a3a750fd45eb0ec990e4ce536c4d2b7
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:29:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Counter from "./components/Counter.jsx";
 export default function App() {
   return (
     <div className="app">
+      {/* Main heading for the Counter App */}
       <h1 className="app-heading">Counter App with Context API</h1>
+      {/* Rendering the Counter component */}
       <Counter />
     </div>
   );
