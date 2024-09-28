f0ebe87604d8a7a522372aaecab5a99164bd4392
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:28:48 +0530

    Update Controls.jsx

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -6,12 +6,15 @@ export default function Controls() {
 
   return (
     <div>
+      {/* Button to increment the counter */}
       <button className="inc-btn btn" onClick={increment}>
         Increment
       </button>
+      {/* Button to decrement the counter */}
       <button className="dec-btn btn" onClick={decrement}>
         Decrement
       </button>
+      {/* Button to reset the counter */}
       <button className="reset-btn btn" onClick={reset}>
         Reset
       </button>
