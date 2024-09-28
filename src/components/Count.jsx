a5bd4c4767107eae9b3e932903b7a82337b67772
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:28:04 +0530

    Update Count.jsx

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import { CounterContext } from "../context/index.jsx";
 
 export default function Count() {
+  // Access the count value from the CounterContext
   const { count } = useContext(CounterContext);
 
   return (
     <div>
+      {/* Display the current count in an h2 element */}
       <h2 className="count">Current Count: {count}</h2>
     </div>
   );
