9b1e65daf9ef7c8960ab84c581cdec1f8979a7ef
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-01 02:27:51 +0530

    Update index.jsx

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -10,7 +10,7 @@ export default function CounterState({ children }) {
   // Functions to modify the count
   const increment = () => setCount((prevCount) => prevCount + 1);
   const decrement = () => setCount((prevCount) => prevCount - 1);
-  const square = () => setCount((prevCount) => prevCount * 2);
+  const square = () => setCount((prevCount) => prevCount ** 2);
   const reset = () => setCount(0);
 
   return (
