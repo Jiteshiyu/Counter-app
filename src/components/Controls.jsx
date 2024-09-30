b58d04c52abfaa7f88a7c646c4b26715fb703e15
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-01 02:12:17 +0530

    Update Controls.jsx

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -6,23 +6,26 @@ export default function Controls() {
 
   return (
     <div>
-      {/* Button to increment the counter */}
-      <button className="inc-btn btn" onClick={increment}>
-        Up
-      </button>
-      {/* Button to decrement the counter */}
-      <button className="dec-btn btn" onClick={decrement}>
-        Down
-      </button>
-      {/* Button to reset the counter */}
-      <button className="reset-btn btn" onClick={reset}>
-        Reset
-      </button>
-    </div>
-    <div>
-      <button className="sq-btn btn" onClick={square}>
-        Get Square
-      </button>
+      <div>
+        {/* Button to increment the counter */}
+        <button className="inc-btn btn" onClick={increment}>
+          Up
+        </button>
+        {/* Button to decrement the counter */}
+        <button className="dec-btn btn" onClick={decrement}>
+          Down
+        </button>
+        {/* Button to reset the counter */}
+        <button className="reset-btn btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
+      <div>
+        {/* Button to square the count */}
+        <button className="sq-btn btn" onClick={square}>
+          Get Square
+        </button>
+      </div>
     </div>
   );
 }
